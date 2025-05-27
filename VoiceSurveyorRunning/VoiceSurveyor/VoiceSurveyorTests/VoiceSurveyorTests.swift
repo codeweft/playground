eb7291b187565745499a1266daa2564caf964c93
prep <prep@intels-MacBook-Pro.local>
2025-05-28 00:13:18 +0800

    Adding Project

---
+++ VoiceSurveyorRunning/VoiceSurveyor/VoiceSurveyorTests/VoiceSurveyorTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceSurveyorTests.swift
+//  VoiceSurveyorTests
+//
+//  Created by prep on 28/5/2025.
+//
+
+import Testing
+@testable import VoiceSurveyor
+
+struct VoiceSurveyorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
